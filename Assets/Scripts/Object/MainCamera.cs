391c80137af2192eb35ba6f70d08701021152d2f
Dmitry <dimon13142@mail.ru>
2020-06-03 22:51:41 +0300

    Movable camera

---
+++ Assets/Scripts/Object/MainCamera.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Sof.Object
+{
+    public class MainCamera : MonoBehaviour
+    {
+        [SerializeField]
+        private float _MoveSpeedByKey;
+        [SerializeField]
+        private MouseButton _MoveMouseButton;
+
+        private Camera _Camera;
+        private Vector3 _PrevMousePos;
+
+        private void Start()
+        {
+            _Camera = GetComponent<Camera>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown((int)_MoveMouseButton))
+                _PrevMousePos = Input.mousePosition;
+
+            if (Input.GetMouseButton((int)_MoveMouseButton))
+            {
+                var mousePos = Input.mousePosition;
+
+                var mouseDelta = _Camera.ScreenToWorldPoint(mousePos) - _Camera.ScreenToWorldPoint(_PrevMousePos);
+                _PrevMousePos = mousePos;
+
+                transform.position = new Vector3(transform.position.x - mouseDelta.x, transform.position.y - mouseDelta.y, transform.position.z);
+
+                return;
+            }
+
+            var delta = _MoveSpeedByKey * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.A))
+                transform.position = new Vector3(transform.position.x - delta, transform.position.y, transform.position.z);
+
+            if (Input.GetKey(KeyCode.W))
+                transform.position = new Vector3(transform.position.x, transform.position.y + delta, transform.position.z);
+
+            if (Input.GetKey(KeyCode.D))
+                transform.position = new Vector3(transform.position.x + delta, transform.position.y, transform.position.z);
+
+            if (Input.GetKey(KeyCode.S))
+                transform.position = new Vector3(transform.position.x, transform.position.y - delta, transform.position.z);
+        }
+    }
+
+}
