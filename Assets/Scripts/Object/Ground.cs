f24f0b9b24f21d13a02eeaa2d93336646cf4835f
Dmitry <dimon13142@mail.ru>
2020-11-22 12:16:53 +0300

    Add tile variance

--- Assets/Scripts/Object/Ground.cs
+++ Assets/Scripts/Object/Ground.cs
@@ -1,8 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace Sof.Object
 {
-    public class Ground : MonoBehaviour
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Ground : Auxiliary.SofSceneMonoBehaviour
     {
+#pragma warning disable 0649
+        [SerializeField]
+        private Sprite[] _Sprites;
+#pragma warning restore 0649
+
+        private void Start()
+        {
+            if (!_Sprites.Any())
+                throw new System.Exception("Sprite array is empty.");
+
+            GetComponent<SpriteRenderer>().sprite = _Sprites[Random.Range(0, _Sprites.Length)];
+        }
     }
 }
